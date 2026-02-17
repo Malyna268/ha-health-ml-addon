ff473da30f075b911f293c813b009d57729e543b
Malyna268 <almotyka@gmail.com>
2026-02-17 19:57:21 +0100

    Update run.sh

--- health_ml/run.sh
+++ health_ml/run.sh
@@ -4,4 +4,4 @@ MQTT_HOST=$(bashio::config 'mqtt_host')
 MQTT_USER=$(bashio::config 'mqtt_user')
 MQTT_PASS=$(bashio::config 'mqtt_pass')
 
-python3 /ml_engine.py "$MQTT_HOST" "$MQTT_USER" "$MQTT_PASS"
+exec python3 /app/ml_engine.py "$MQTT_HOST" "$MQTT_USER" "$MQTT_PASS"
