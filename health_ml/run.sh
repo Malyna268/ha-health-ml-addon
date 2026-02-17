7b21fd7d29edaea5675536444df2f00cdcb6a9a2
Malyna268 <almotyka@gmail.com>
2026-02-17 21:58:47 +0100

    Delete health_ml/run.sh

--- health_ml/run.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/with-contenv bashio
-
-MQTT_HOST=$(bashio::config 'mqtt_host')
-MQTT_USER=$(bashio::config 'mqtt_user')
-MQTT_PASS=$(bashio::config 'mqtt_pass')
-
-exec python3 /app/ml_engine.py "$MQTT_HOST" "$MQTT_USER" "$MQTT_PASS"
